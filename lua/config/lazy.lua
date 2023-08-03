5405a2792a2ab401f642ad3e0e77e956f00ee250
Po-Yeh Chen <poyehchen@cs.nctu.edu.tw>
2023-08-03 21:44:42 +0800

    Remove go module

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -13,7 +13,7 @@ require("lazy").setup({
     -- import any extras modules here
     { import = "lazyvim.plugins.extras.lang.clangd" },
     { import = "lazyvim.plugins.extras.lang.docker" },
-    { import = "lazyvim.plugins.extras.lang.go" },
+    -- { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.rust" },
