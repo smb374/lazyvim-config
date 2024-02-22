4177bbfaf2cdb6f992e24991b82ab2d7c31a0372
Po-Yeh Chen <poyehchen@cs.nycu.edu.tw>
2024-02-22 08:36:46 +0800

    Update cmp setting & disable auto plugin update check.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -30,7 +30,7 @@ require("lazy").setup({
       not_loaded = "",
     },
   },
-  checker = { enabled = true }, -- automatically check for plugin updates
+  checker = { enabled = false }, -- automatically check for plugin updates
   performance = {
     rtp = {
       -- disable some rtp plugins
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -32,10 +32,10 @@ return {
             cmp.select_next_item()
           -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
           -- they way you will only jump inside the snippet region
-          -- elseif luasnip.expand_or_jumpable() then
-          --   luasnip.expand_or_jump()
-          -- elseif has_words_before() then
-          --   cmp.complete()
+          elseif luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
           else
             fallback()
           end
@@ -43,8 +43,8 @@ return {
         ["<S-Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             cmp.select_prev_item()
-          -- elseif luasnip.jumpable(-1) then
-          --   luasnip.jump(-1)
+          elseif luasnip.in_snippet() and luasnip.jumpable(-1) then
+            luasnip.jump(-1)
           else
             fallback()
           end
